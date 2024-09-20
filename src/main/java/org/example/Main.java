ce653ada8273e2dd7b6509fd0897df75377fe3ae
mohammed mesbahi <116631139+mohammedMesbahi@users.noreply.github.com>
2024-09-20 15:19:21 +0100

    save

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,19 @@
 package org.example;
 
-import org.example.addBinary.*;
+import org.example.binaryTreePaths.*;
 
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
         System.out.println(
-                s.addBinary("1010","1011")
+                s.binaryTreePaths(
+                        root
+                )
         );
     }
 }
--- src/main/java/org/example/addBinary/Solution.java
+++ src/main/java/org/example/addBinary/Solution.java
@@ -2,17 +2,27 @@ package org.example.addBinary;
 
 public class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() - 1;
-        int carry = 0;
-        while (i > -1 || j > -1) {
-            int sum = carry;
-            if (i > -1) sum += a.charAt(i--) - '0';
-            if (j > -1) sum += b.charAt(j--) - '0';
-            sb.append(sum % 2);
-            carry = sum / 2;
+        char[] ch = new char[Math.max(a.length(), b.length()) + 1];
+        int e = ch.length - 1;
+        int carray = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) {
+                carray += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                carray += b.charAt(j) - '0';
+                j--;
+            }
+            ch[e--] = (char) ('0' + carray % 2);
+            carray = carray / 2;
         }
-        if (carry != 0) sb.append(carry);
-        return sb.reverse().toString();
+        if (carray == 1) {
+            ch[0] = '1';
+            return new String(ch);
+        }
+        return new String(ch, 1, ch.length - 1);
     }
 }
