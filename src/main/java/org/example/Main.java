75f6335ac0eff537e3f507819d3a1c004eb046ed
mohammed mesbahi <116631139+mohammedMesbahi@users.noreply.github.com>
2024-11-03 00:52:35 +0100

    maxScore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
-import org.example.freqAlphabets.*;
+import org.example.destCity.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.freqAlphabets("1326#"));
+        List<List<String>> list = new ArrayList<>();
+        list.add(List.of("London", "New York"));
+        list.add(List.of("New York", "Lima"));
+        list.add(List.of("Lima", "Sao Paulo"));
+        System.out.println(solution.destCity(list));
     }
 }
--- src/main/java/org/example/maxScore/Solution.java
+++ src/main/java/org/example/maxScore/Solution.java
@@ -2,26 +2,23 @@ package org.example.maxScore;
 
 class Solution {
     public int maxScore(String s) {
-        int maximumScore = 0;
         char[] arr = s.toCharArray();
-        int zeros = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == '0') zeros++;
-        } // 00 01
-        int i = arr.length - 1;
-        int ones = arr[i] - '0';
+        int zeros = arr[0] == '0' ? 1 : 0;
+        int ones = 0;
 
-        while (true) {
-            maximumScore = Math.max(ones + zeros, maximumScore);
-            i--;
-            if (arr[i] == '1') {
-                ones++;
+        int maxOfZeros = zeros;
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] == '0') {
+                zeros++;
             } else {
-                zeros = Math.max(--zeros, 0);
+                ones++;
+                zeros--;
             }
-            if (i == 0) break;
+            maxOfZeros = Math.max(maxOfZeros, zeros);
         }
-        return maximumScore;
+        ones += arr[arr.length - 1] - '0';
 
+        return maxOfZeros + ones;
     }
 }
