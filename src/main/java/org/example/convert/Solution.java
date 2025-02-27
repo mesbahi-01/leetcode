614cd1c715a32afa76759a86f18e2152a1d67542
mesbahi <mesbahi.ed@outlook.com>
2025-02-27 20:31:11 +0000

    save

--- src/main/java/org/example/convert/Solution.java
+++ src/main/java/org/example/convert/Solution.java
@@ -6,41 +6,20 @@ class Solution {
     }
 
     public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
         StringBuilder result = new StringBuilder(s.length());
-        int numberOfColumns = s.length();
 
-        char[][] matrix = new char[numRows][numberOfColumns];
-        for (int i = 0; i < numRows; i++) {
-            for (int col = 0; col < numberOfColumns; col++) {
-                matrix[i][col] = Character.MIN_VALUE;
+        for (int r = 0; r < numRows; r++) {
+            int increment = (numRows - 1) * 2;
+
+            for (int i = r; i < s.length(); i += increment) {
+                result.append(s.charAt(i));
+
+                if (r > 0 && r < numRows - 1 && i + increment - 2 * r < s.length()) {
+                    result.append(s.charAt(i + increment - 2 * r));
+                }
             }
         }
-
-        int column = 0;
-        int k = 0;
-
-        while (k < s.length()) {
-            for (int i = 0; i < numRows && k < s.length(); i++) {
-                matrix[i][column] = s.charAt(k);
-                k++;
-            }
-
-            for (int i = numRows - 2; i > 0 && k < s.length(); i--) {
-                column++;
-                matrix[i][column] = s.charAt(k);
-                k++;
-            }
-            column++;
-        }
-
-
-        for (int row = 0; row < numRows; row++) {
-            for (column = 0; column < numberOfColumns; column++) {
-                if (matrix[row][column] != Character.MIN_VALUE)
-                    result.append(matrix[row][column]);
-            }
-        }
-
         return result.toString();
     }
 }
