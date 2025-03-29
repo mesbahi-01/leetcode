12b2d1bb620b5235a7e8bd78bff73d037412f91f
mesbahi <mesbahi.ed@outlook.com>
2025-03-29 21:01:58 +0000

    save

--- src/main/java/org/example/productExceptSelf/Solution.java
+++ src/main/java/org/example/productExceptSelf/Solution.java
@@ -1,46 +1,48 @@
 package org.example.productExceptSelf;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int product = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            product *= nums[i];
-        }
-
-        int numberOfZeros = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) numberOfZeros++;
-            if (numberOfZeros == 2) break;
-        }
-
-        // found number of zeros more than 2
-        // everything is 0 in output array
-
         int[] output = new int[nums.length];
 
-        int newProduct = 0;
-
-        if (numberOfZeros > 1) {
-            Arrays.fill(output, 0);
-            return output;
-        } else if (numberOfZeros == 1) {
-            newProduct = nums[0];
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] != 0) newProduct *= nums[i];
-            }
-        }
+        Map<Integer, Integer> index_totalProductOfUpcomingValues_map = new HashMap<>();
+        Map<Integer, Integer> index_totalProductOfPreviousValues_map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                output[i] = newProduct;
-            } else {
-                output[i] = product / nums[i];
-            }
+            output[i] = calculateProductOfPreviousValues(nums, i, index_totalProductOfPreviousValues_map)
+                    * calculateProductOfUpcomingValues(nums, i, index_totalProductOfUpcomingValues_map);
         }
+
+
         return output;
+    }
+
+    private int calculateProductOfUpcomingValues(int[] nums, int i, Map<Integer, Integer> indexTotalProductOfUpcomingValuesMap) {
+        if (i >=nums.length - 1 ) return 1;
+        else {
+            indexTotalProductOfUpcomingValuesMap.put(
+                    i,
+                    nums[i + 1] *
+                            calculateProductOfUpcomingValues(nums,i + 1,indexTotalProductOfUpcomingValuesMap)
+            );
+
+            return indexTotalProductOfUpcomingValuesMap.get(i);
+        }
+
+    }
+
+    private int calculateProductOfPreviousValues(int[] nums, int i, Map<Integer, Integer> indexTotalProductOfPreviousValues_map) {
+        if (i <= 0) return 1;
+        else {
+            indexTotalProductOfPreviousValues_map.put(
+                    i,
+                    nums[i - 1] * calculateProductOfPreviousValues(nums, i - 1, indexTotalProductOfPreviousValues_map)
+            );
+        }
+        return indexTotalProductOfPreviousValues_map.get(i);
     }
 }  
